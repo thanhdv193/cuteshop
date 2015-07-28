0ccccae8a6b65b773e8ee349eba028b0b0ce7563
thanhdv <thanhdv193@gmail.com>
2015-07-28 09:19:11 +0700

    update config/db.php

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=basic',
+    'dsn' => 'mysql:host=localhost;dbname=cuteshop',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
