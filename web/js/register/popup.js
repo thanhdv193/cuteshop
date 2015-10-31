a965422cc2c9b12e9d7054f18fc65c2c8d1fce97
thanhdv <thanhdv193@gmail.com>
2015-10-31 12:13:17 +0700

    update

--- web/js/register/popup.js
+++ web/js/register/popup.js
@@ -32,40 +32,56 @@ jQuery(document).ready(function () {
             url: 'http://localhost:803/site/login-ajax',
             cache: false,
             type: 'post',
-            data: {username: username , password :password},
+            data: {username: username, password: password},
             success: function (result)
-            {   if(result == 'user_name_null')
+            {
+                if (result == 'user_name_null')
                 {
                     $('#login-error').text('Tên đăng nhập không được trống.');
-                }   
-                if(result == 'password_name_null')
+                }
+                if (result == 'password_name_null')
                 {
                     $('#login-error').text('Mật khẩu không được trống.');
                 }
-                if(result == 'user_not_exist')
+                if (result == 'user_not_exist')
                 {
                     $('#login-error').text('Tài khoản không tồn tại.');
                 }
-                if(result == 'password_exist')
+                if (result == 'password_exist')
                 {
                     $('#login-error').text('Mật khẩu chưa đúng.');
                 }
-                if(result == 'successful')
+                if (result == 'successful')
                 {
                     $('#close_p').click();
                 }
             }
         });
     }
-    $('#login-button').on('click', function(e){
+    $('#login-button').on('click', function (e) {
         e.preventDefault();
         login();
-        
+
     });
-    $('.close_popup').on('click', function(e){
+    $('.close_popup').on('click', function (e) {
         e.preventDefault();
-        
-        $('#basicModal').css('display','none');
+
+        $('#basicModal').css('display', 'none');
+
+    });
+
+    // Toggle Function
+    
+    $('.register').click(function () { 
+        // Switches the Icon
+        //$(this).children('i').toggleClass('fa-pencil');
+        // Switches the forms  
+        $('.form').animate({
+            height: "toggle",
+            'padding-top': 'toggle',
+            'padding-bottom': 'toggle',
+            opacity: "toggle"
+        }, "slow");
         
     });
 })
