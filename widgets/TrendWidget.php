d8fb521cbb5160ce63773c049287a097b7d7d3f5
thanhdv <thanhdv193@gmail.com>
2015-11-10 19:52:25 +0700

    1234

--- widgets/TrendWidget.php
+++ widgets/TrendWidget.php
@@ -16,21 +16,21 @@ class TrendWidget extends Widget
     public function run()
     {
         $listproduct = Product::find()->where(['product_group_id'=>1,'active'=>1])->asArray()->all();
-        $listId = array();
-        foreach($listproduct as $value)
-        {
-            $listId[] = $value['product_id'];
-        }
-        $imageProduct = Image::find()->where(['object_id'=>$listId])->all();
-        $arrayImage = array();
-        foreach($imageProduct as $value)
-        {
-            $arrayImage[$value['object_id']] = $value;
-        }
-        foreach($listproduct as &$value)
-        {
-            $value['image_list'] = $arrayImage[$value['product_id']];
-        }
+//        $listId = array();
+//        foreach($listproduct as $value)
+//        {
+//            $listId[] = $value['product_id'];
+//        }
+//        $imageProduct = Image::find()->where(['object_id'=>$listId])->all();
+//        $arrayImage = array();
+//        foreach($imageProduct as $value)
+//        {
+//            $arrayImage[$value['object_id']] = $value;
+//        }
+//        foreach($listproduct as &$value)
+//        {
+//            $value['image_list'] = $arrayImage[$value['product_id']];
+//        }
         //echo '<pre>'; var_dump($listproduct); die;
         return $this->render('Trend',['listproduct'=>$listproduct]);      
     }
